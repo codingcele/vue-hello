f4fe429b6779be37a4511730c8cb30802aa01e69
codingcele <marcocelegato9@gmail.com>
2022-11-25 18:07:15 +0100

    link img sistemato

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ let app = new Vue({
     el:"#app",
     data: {
         message: "Questo è un gatto:",
-        linkImg: "/Users/celeg/Documents/Corso BOOLEAN/vue-hello/img/download.jpg"
+        linkImg: "img/download.jpg"
     }
 })
