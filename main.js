ebd9323d4e5767d040bb5e8871bb5bf01b4cecbd
codingcele <marcocelegato9@gmail.com>
2022-11-25 17:28:52 +0100

    finito

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let app = new Vue({
+    el:"#app",
+    data: {
+        message: "Questo è un gatto:",
+        linkImg: "/Users/celeg/Documents/Corso BOOLEAN/vue-hello/img/download.jpg"
+    }
+})
